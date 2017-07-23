7f3ac58a788b2433a7e16e46f536015ea60f234b
Sam Denty <samddenty@gmail.com>
2017-07-23 15:37:28 +0100

    Added search page

--- js/custom.js
+++ js/custom.js
@@ -2,24 +2,24 @@
 	$(document).ready(function() {
 	    $(".button-collapse").sideNav();
 	    if ($(window).width() > 993) {
-		    $(window).on('scroll', function(){checkScroll()});
-		    $("html").on("click", "body.tight .main-wrap", function() {
-		        $('html, body').animate({
-		            scrollTop: $('.main-wrap').outerHeight() - $(window).height()
-		        }, 500);
-		    });
-		   	checkScroll();
-		}
-		$('.parallax').parallax();
+	        $(window).on('scroll', function() { checkScroll() });
+	        $("html").on("click", "body.tight .main-wrap", function() {
+	            $('html, body').animate({
+	                scrollTop: $('.main-wrap').outerHeight() - $(window).height()
+	            }, 500);
+	        });
+	        checkScroll();
+	    }
+	    $('.parallax').parallax();
 	})
 	function checkScroll() {
-		if ($(window).scrollTop() + $(window).height() > $('.main-wrap').outerHeight() + 60) {
-		    $('body').addClass('tight');
-		    $('.side-nav').addClass('dn');
-		} else {
-		    $('body').removeClass('tight');
-		    setTimeout("$('.side-nav').removeClass('dn');", 500);
-		}
+	    if ($(window).scrollTop() + $(window).height() > $('.main-wrap').outerHeight() + 60) {
+	        $('body').addClass('tight');
+	        $('.side-nav').addClass('dn');
+	    } else {
+	        $('body').removeClass('tight');
+	        setTimeout("$('.side-nav').removeClass('dn');", 500);
+	    }
 	}
 /* Enable the header transparency effect */
 	var header = $("nav");
@@ -35,7 +35,24 @@
 	    });
 	};
 /* Initiate contact popup */
-	$(document).ready(function(){
-		$('.modal').modal();
-		$('.carousel').carousel();
+	$(document).ready(function() {
+	    $('.modal').modal();
+	    $('.carousel').carousel();
 	});
+/* Custom search dialog */
+	var query = document.getElementById("searchQuery");
+	function submitSearch() {
+	    window.location = "search?" + query.value;
+	}
+	function prepareSearch() {
+	    setTimeout(function() { query.focus(); }, 200);
+	    query.onkeypress = function(e) {
+	        var event = e || window.event;
+	        var charCode = event.which || event.keyCode;
+
+	        if (charCode == '13') {
+	            submitSearch();
+	            return false;
+	        }
+	    }
+	}
---
+++ js/search.js
@@ -0,0 +1,21 @@
+var search = window.location.href;
+var string = document.getElementById("web-search-string");
+var iframe = document.getElementById("web-search");
+var n = search.indexOf('?');
+search = search.substring(n + 1);
+string.value = search;
+iframe.src = "https://duckduckgo.com/?q=" + search + "%3Asamdd.me%2Cmirum.weebly.com&kp=-2&kh=1&ko=-2&k1=-1&kv=-1&kn=1&kac=-1&kaf=1";
+
+function webSearch() {
+    search = string.value;
+    iframe.src = "https://duckduckgo.com/?q=" + search + "%3Asamdd.me%2Cmirum.weebly.com&kp=-2&kh=1&ko=-2&k1=-1&kv=-1&kn=1&kac=-1&kaf=1";
+}
+document.getElementById('web-search-string').onkeypress = function(e) {
+    var event = e || window.event;
+    var charCode = event.which || event.keyCode;
+
+    if (charCode == '13') {
+        webSearch();
+        return false;
+    }
+}
