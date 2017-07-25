772ba6571a04e1f74f72ad3193a3b069145f8597
Sam Denty <sam@samdd.me>
2017-07-25 12:29:33 +0100

    Removed template 1

--- website-template_v1/Start Server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call start "Mini-Webserver" cmd /c "mode con: cols=50 lines=7&color 0a&cls&echo.&echo   Mini-Webserver is running on port 80&echo  Close this window to stop the server&echo.&echo          http://127.0.0.1:80&webserver "%cd%" "%port%""
-start http://127.0.0.1:80
