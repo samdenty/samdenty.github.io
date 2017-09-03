f7318aba40ce695012fe8c53142ee6e9184ac926
samdenty99 <sam@samdd.me>
2017-09-03 08:53:40 +0100

    Bug fix

--- js/search.js
+++ js/search.js
@@ -9,11 +9,11 @@ else
 	search = search.substring(n + 1)
 
 string.value = search;
-iframe.src = "https://duckduckgo.com/?q=site:mirum.weebly.com,samdd.me" + search + "&kp=-2&kh=1&ko=-2&k1=-1&kv=-1&kn=1&kac=-1&kaf=1";
+iframe.src = "https://duckduckgo.com/?q=site:samdd.me " + search + "&kp=-2&kh=1&ko=-2&k1=-1&kv=-1&kn=1&kac=-1&kaf=1";
 
 function webSearch() {
     search = string.value;
-    iframe.src = "https://duckduckgo.com/?q=site:mirum.weebly.com,samdd.me" + search + "&kp=-2&kh=1&ko=-2&k1=-1&kv=-1&kn=1&kac=-1&kaf=1";
+    iframe.src = "https://duckduckgo.com/?q=site:samdd.me " + search + "&kp=-2&kh=1&ko=-2&k1=-1&kv=-1&kn=1&kac=-1&kaf=1";
 }
 
 document.getElementById('web-search-string').onkeypress = function(e) {
