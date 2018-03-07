219599155d82e80cb39843c6c507c65f53be1853
samdd <sam@samdd.me>
2018-03-07 16:12:48 +0000

    *

--- js/sc.js
+++ js/sc.js
@@ -3,8 +3,8 @@ _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function () {
 	var u = "//analytics.samdd.me/";
-	_paq.push(['setTrackerUrl', u + 'piwik.php']);
+	_paq.push(['setTrackerUrl', u + 'php']);
 	_paq.push(['setSiteId', '1']);
 	var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-	g.type = 'text/javascript'; g.async = true; g.defer = true; g.src = u + 'piwik.js'; s.parentNode.insertBefore(g, s);
+	g.type = 'text/javascript'; g.async = true; g.defer = true; g.src = u + 'js'; s.parentNode.insertBefore(g, s);
 })()
